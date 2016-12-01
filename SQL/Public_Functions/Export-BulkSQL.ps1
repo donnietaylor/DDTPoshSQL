59bb8e787cb9069ce982535b8896a1fef7bd0080
Donnie Taylor <draith@outlook.com>
2016-12-01 15:33:40 -0600

    Add files via upload

---
+++ SQL/Public_Functions/Export-BulkSQL.ps1
@@ -0,0 +1,142 @@
+﻿<#
+	.SYNOPSIS
+		Exports a datatable into a SQL table
+	
+	.DESCRIPTION
+		This function bulk copies a PowerShell datatable into a SQL table.  Bulk copies are tricky - the datatable (and the data) must be properly formatted exactly to match the destination SQL table.
+	
+	.PARAMETER sqlserver
+		Name of the SQL server that contains the destination database and table.
+	
+	.PARAMETER sqldatabase
+		Database that contains the SQL Table that will be the destination of the bulk export.
+	
+	.PARAMETER sqluser
+		Optional SQL User that will be used to establish a connection the SQL database that houses the destination table.
+	
+	.PARAMETER sqlpass
+		Password of the SQL user that will be used to establish a connection to the destination SQL table.
+	
+	.PARAMETER sqlintegratedsecurity
+		If specified as true, the username and password parameters are not used.  Instead, the connection will use IntegratedSecurity to establish a connection to the destination SQL server.  If set to false, sqluser and sqlpass are needed.
+	
+	.PARAMETER datatable
+		The PowerShell datatable that is used as the source of bulk export to the SQL table.
+	
+	.PARAMETER sqltable
+		The name of the SQL table that is the target of the bulk export.
+	
+	.PARAMETER failover
+		Specifies if the connection should use the 'MultiSubnetFailover' connection option, which is mandatory for connections to SQL AlwaysOn Availability Groups.  Requires .net 4.5 or greater.
+	
+	.EXAMPLE
+		PS C:\> Export-BulkSQL -sqlserver $value1
+	
+	.NOTES
+		Additional information about the function.
+#>
+function Export-BulkSQL
+{
+	[CmdletBinding(PositionalBinding = $true)]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true,
+				   Position = 1)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('server', 'databaseserver')]
+		[string]$sqlserver,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('database')]
+		[string]$sqldatabase,
+		[Parameter(Mandatory = $false,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('user')]
+		[string]$sqluser,
+		[Parameter(Mandatory = $false,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('pass', 'password')]
+		[string]$sqlpass,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateSet('true', 'false')]
+		[Alias('integratedsecurity')]
+		[string]$sqlintegratedsecurity,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		$datatable,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$sqltable,
+		[Parameter(ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateSet('true', 'false')]
+		[string]$failover = 'false'
+	)
+	
+	Begin
+	{
+		
+	}
+	Process
+	{
+		$SqlConnection = New-Object -TypeName System.Data.SqlClient.SqlConnection
+		if ($sqlintegratedsecurity -eq $false)
+		{
+			$SqlConnection.ConnectionString = "Server = $sqlserver; Initial Catalog = $sqldatabase; Integrated Security = FALSE ;User ID = $sqluser ;Password = $sqlpass;"
+		}
+		else
+		{
+			$SqlConnection.ConnectionString = "Server = $sqlserver; Initial Catalog = $sqldatabase; Integrated Security = TRUE;"
+		}
+		if ($failover -eq $true)
+		{
+			$SqlConnection.ConnectionString = $SqlConnection.ConnectionString + "multisubnetfailover = TRUE;"
+		}
+		Write-Verbose $SqlConnection.ConnectionString
+		try
+		{
+			$SqlConnection.Open()
+		}
+		catch
+		{
+			Write-Verbose "Open connection failed"
+		}
+		try
+		{
+			$BulkCopy = new-object ("System.Data.SqlClient.SqlBulkCopy") $SqlConnection
+			$BulkCopy.DestinationTableName = "$sqltable"
+			$BulkCopy.WriteToServer($datatable)
+		}
+		catch
+		{
+			Write-Verbose "Bulk Insert failed"
+		}
+		$SqlConnection.Close()
+	}
+	End
+	{
+		
+	}
+}
---
+++ SQL/Public_Functions/Invoke-SQL.ps1
@@ -0,0 +1,147 @@
+﻿#requires -Version 2
+<#
+	.SYNOPSIS
+		Connects to MS SQL server and performs a sql statement on the proper table.
+	
+	.DESCRIPTION
+		Connects to MS SQL server and performs a sql statement on the proper table.
+	
+	.PARAMETER server
+		Name of the server hosting the database.  If you specify AlwaysOn, then the servername should be the name of the AlwaysOn Listener
+	
+	.PARAMETER database
+		Name of the SQL Database
+	
+	.PARAMETER user
+		User with appropriate permissions to the database. Ignored if the integratedsecurity is set to $TRUE
+	
+	.PARAMETER password
+		Password for the user that has appropriate permissions to the database.  Ignored if the integratedsecurity is set to $TRUE
+	
+	.PARAMETER statement
+		Query, update, exec, or insert statement to be performed against the database.
+	
+	.PARAMETER method
+		Enter "query" if returning data (such as select statement), otherwise enter update, insert, or exec.
+	
+	.PARAMETER integratedsecurity
+		$FALSE if using a username and password, otherwise $TRUE to use the account executing the script.
+	
+	.PARAMETER alwayson
+		If connecting to a SQL AlwaysOn Availability Group, enter $TRUE, and specify the name of the listener as the server paramter.  Otherwise, leave blank or enter $false
+	
+	.EXAMPLE
+		Invoke-SQL [-server] <Object> [-database] <Object> [-user] <Object> [-password] <Object> [-statement] <Object> [-method] <Object>  [<CommonParameters>]
+	
+	.NOTES
+		Additional information about the function.
+#>
+function Invoke-SQL
+{
+	[CmdletBinding(PositionalBinding = $true,
+				   SupportsPaging = $true,
+				   SupportsShouldProcess = $true)]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('sqlserver')]
+		[string]$server,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('sqldatabase')]
+		[string]$database,
+		[Parameter(Mandatory = $false,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[Alias('username', 'sqluser', 'sqlusername')]
+		[string]$user,
+		[Parameter(Mandatory = $false,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[Alias('pass', 'sqlpass', 'sqlpassword')]
+		[string]$password,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('sqlstatement')]
+		[string]$statement,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$method,
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('sqlintegratedsecurity')]
+		[bool]$integratedsecurity,
+		[Parameter(ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true)]
+		[Alias('multisubnetfailover')]
+		[bool]$alwayson = $false
+	)
+	
+	begin { }
+	Process
+	{
+		Write-Verbose "Statement: $statement"
+		try
+		{
+			$SqlConnection = New-Object -TypeName System.Data.SqlClient.SqlConnection
+			if ($integratedsecurity -eq $false)
+			{
+				$SqlConnection.ConnectionString = "Server = $server; Initial Catalog = $database; Integrated Security = FALSE ;User ID = $user ;Password = $password;"
+			}
+			else
+			{
+				$SqlConnection.ConnectionString = "Server = $server; Initial Catalog = $database; Integrated Security = TRUE;"
+			}
+			if ($alwayson -eq $true)
+			{
+				$SqlConnection.ConnectionString = $SqlConnection.ConnectionString + 'multisubnetfailover = TRUE;'
+			}
+			Write-Verbose $SqlConnection.ConnectionString
+			$SqlCmd = New-Object -TypeName System.Data.SqlClient.SqlCommand
+			$SqlCmd.CommandText = $statement
+			$SqlCmd.Connection = $SqlConnection
+			$SqlCmd.CommandTimeout = 0
+			$SqlAdapter = New-Object -TypeName System.Data.SqlClient.SqlDataAdapter
+			$SqlAdapter.SelectCommand = $SqlCmd
+			$SqlConnection.Open()
+			if ($method -eq 'query')
+			{
+				$DataSet = New-Object -TypeName System.Data.DataSet
+				$null = $SqlAdapter.Fill($DataSet)
+				write-output $DataSet
+			}
+			else
+			{
+				$null = $SqlCmd.ExecuteNonQuery()
+			}
+			$SqlConnection.close()
+		}
+		catch
+		{
+			Write-verbose "Error: $statement"
+			Write-verbose  $_
+		}
+	}
+	end { }
+}
+
+
